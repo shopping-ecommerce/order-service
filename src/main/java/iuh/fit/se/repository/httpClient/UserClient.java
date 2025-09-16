9755ba4915dc13f77cb76e07682292147067d755
thinh <21135681.thinh@student.iuh.edu.vn>
2025-09-17 04:22:33 +0700

    Config kafka from order-service to product-service

--- src/main/java/iuh/fit/se/repository/httpClient/UserClient.java
+++ src/main/java/iuh/fit/se/repository/httpClient/UserClient.java
@@ -3,6 +3,7 @@ package iuh.fit.se.repository.httpClient;
 import iuh.fit.se.configuration.AuthenticationRequestInterceptor;
 import iuh.fit.se.dto.response.ApiResponse;
 import iuh.fit.se.dto.response.AuthResponse;
+import iuh.fit.se.dto.response.SellerResponse;
 import iuh.fit.se.dto.response.UserResponse;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -15,4 +16,6 @@ public interface UserClient {
     // GET /profiles/{id}
     @GetMapping("/profiles/{id}")
     ApiResponse<UserResponse> getUserById(@PathVariable("id") String id);
+    @GetMapping("sellers/searchBySellerId/{sellerId}")
+    ApiResponse<SellerResponse> searchBySellerId(@PathVariable("sellerId") String sellerId);
 }
